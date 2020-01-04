ac88e66655d79091f797af9b035ebc80af717b7e
Lucas <lucashmccabe@gmail.com>
2020-01-04 12:49:20 -0500

    Sock Merchant problem and README

---
+++ HackerRank/Warm-up Challenges/SockMerchant.py
@@ -0,0 +1,24 @@
+#https://www.hackerrank.com/challenges/sock-merchant/
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    return sum([ar.count(color)//2 for color in set(ar)])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-LeetYeet
+# LeetYeet
+
+A place for me to put solutions to various coding challenge problems.
+
+## Index
+* [HackerRank](#hackerrank)
+
+
+
+### HackerRank
+* [Warm-up Challenges](https://github.com/lucasmccabe/LeetYeet/HackerRank/Warm-up Challenges)
+
+
+
+## License
+[The Unlicense](https://choosealicense.com/licenses/unlicense/)
