336582128e1b0fce7826a972dc3f032785b3749a
Lucas <lucashmccabe@gmail.com>
2020-01-08 08:13:11 -0500

    File organization.

---
+++ HackerRank/30 Days of Code/Data_Types.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+intVar, doubVar, strVar = None, None, None
+# Read and save an integer, double, and String to your variables.
+intVar = int(input())
+doubVar = float(input())
+strVar = str(input())
+# Print the sum of both integer variables on a new line.
+print(i+intVar)
+# Print the sum of the double variables on a new line.
+print(d+doubVar)
+# Concatenate and print the String variables on a new line
+print(s+strVar)
+# The 's' variable above should be printed first.
+
