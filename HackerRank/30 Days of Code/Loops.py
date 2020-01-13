d5fca02b56afff4f8f5a5b0e5009547b31aabd57
Lucas <lucashmccabe@gmail.com>
2020-01-12 22:35:23 -0500

    Added three new challenge solutions.

---
+++ HackerRank/30 Days of Code/Loops.py
@@ -0,0 +1,14 @@
+#https://www.hackerrank.com/challenges/30-loops/
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,11):
+        print('%d x %d = %d' %(n, i, n*i))
---
+++ HackerRank/Algorithms/Equalize_the_Array.py
@@ -0,0 +1,24 @@
+#https://www.hackerrank.com/challenges/equality-in-a-array
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the equalizeArray function below.
+def equalizeArray(arr):
+    return len(arr)-max(arr.count(i) for i in arr)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
